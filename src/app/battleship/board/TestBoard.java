8d9cca33e2a9ab27bc687aa3c913a3b1c94157cc
Isidore PORRETTI <isidore.porretti@epfedu.fr>
2021-11-14 13:58:55 +0100

    ex3-iboard

--- src/app/battleship/board/TestBoard.java
+++ src/app/battleship/board/TestBoard.java
@@ -1,7 +1,15 @@
 package app.battleship.board;
 
+
+import app.battleship.ship.*;
+import app.battleship.ship.AbstractShip.Orientation;
+
 public class TestBoard {
     public static void main(String args[]) {
-		new Board("test").print();
+        Board b = new Board("test");
+		b.putHit(1, 2);
+		b.putShip(new Carrier(Orientation.NORTH), 4, 4);
+		b.putShip(new Destroyer(Orientation.EAST), 5, 4);
+		b.print();
 	}
 }
